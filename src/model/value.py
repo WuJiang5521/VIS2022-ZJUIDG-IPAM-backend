666993876db2bde1b2b0d96db384d12663a94b7f
guoziyang <gzygzy991030@yahoo.com>
2022-02-17 18:49:16 +0800

    delete numerical value

--- src/model/value.py
+++ src/model/value.py
@@ -3,14 +3,6 @@ from typing import Tuple, Dict
 from pydantic import BaseModel
 
 
-class Value(BaseModel):
-    num_value: int
-    name: str
+Value = str
 
-
-class Freq(BaseModel):
-    num_value: int
-    freq: int
-
-
-FreqValue = Dict[str, Freq]  # {name: (num_value, freq)}
+FreqValue = Dict[str, int]  # {name: freq}
